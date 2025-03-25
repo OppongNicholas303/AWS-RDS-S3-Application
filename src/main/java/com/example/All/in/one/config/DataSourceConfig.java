f3c02fc93b81186baf54ae8c043488514af7b232
nicholas <nicholas.oppong@amalitech.com>
2025-03-25 20:19:17 +0000

    modify configuration

--- src/main/java/com/example/All/in/one/config/DataSourceConfig.java
+++ src/main/java/com/example/All/in/one/config/DataSourceConfig.java
@@ -1,176 +1,68 @@
-//package com.example.All.in.one.config;
-//
-//import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
-//import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
-//import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class DataSourceConfig {
-//    private static final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
-//
-//    private final AWSSimpleSystemsManagement ssmClient;
-//
-//    @Value("${aws.ssm.db-endpoint-param}")
-//    private String dbEndpointParam;
-//
-//    @Value("${aws.ssm.db-name-param}")
-//    private String dbNameParam;
-//
-//    @Value("${aws.ssm.db-username-param}")
-//    private String dbUsernameParam;
-//
-//    @Value("${aws.ssm.db-password-param}")
-//    private String dbPasswordParam;
-//
-//    @Value("${spring.datasource.timeout:30000}")  // Default 30 seconds timeout
-//    private int connectionTimeout;
-//
-//    public DataSourceConfig() {
-//        this.ssmClient = AWSSimpleSystemsManagementClientBuilder.standard()
-//                .build();
-//    }
-//
-//    private String getParameterValue(String parameterName) {
-//        try {
-//            GetParameterRequest request = new GetParameterRequest()
-//                    .withName(parameterName)
-//                    .withWithDecryption(true);
-//
-//            String value = ssmClient.getParameter(request).getParameter().getValue();
-//            logger.info("Successfully retrieved parameter: {}", parameterName);
-//            return value;
-//        } catch (Exception e) {
-//            logger.error("Error retrieving parameter {}: {}", parameterName, e.getMessage());
-//            throw new RuntimeException("Could not retrieve database parameter: " + parameterName, e);
-//        }
-//    }
-//
-//    @Bean
-//    @Primary
-//    public DataSource dataSource() {
-//        try {
-//            String endpoint = getParameterValue(dbEndpointParam);
-//            String dbName = getParameterValue(dbNameParam);
-//            String username = getParameterValue(dbUsernameParam);
-//            String password = getParameterValue(dbPasswordParam);
-//
-//            logger.info("Configuring DataSource with endpoint: {}, database: {}", endpoint, dbName);
-//
-////            String jdbcUrl = String.format("jdbc:postgresql://%s:5432/%s?connectTimeout=%d",
-////                    endpoint, dbName, connectionTimeout);
-//
-//            String jdbcUrl = String.format("jdbc:postgresql://%s:5432/%s?connectTimeout=%d&ssl=true",
-//                    endpoint, dbName, connectionTimeout);
-//
-//            return DataSourceBuilder.create()
-//                    .url(jdbcUrl)
-//                    .username(username)
-//                    .password(password)
-//                    .driverClassName("org.postgresql.Driver")
-//                    .build();
-//        } catch (Exception e) {
-//            logger.error("Failed to create DataSource", e);
-//            throw new RuntimeException("Could not create DataSource", e);
-//        }
-//    }
-//}
-
 package com.example.All.in.one.config;
 
-import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
-import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
-import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import com.zaxxer.hikari.HikariDataSource;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.regions.Region;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    private static final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
 
-    private final AWSSimpleSystemsManagement ssmClient;
-
-    @Value("${aws.ssm.db-endpoint-param:/s3-image-upload-app/database/endpoint}")
-    private String dbEndpointParam;
-
-    @Value("${aws.ssm.db-name-param:/s3-image-upload-app/database/name}")
-    private String dbNameParam;
-
-    @Value("${aws.ssm.db-username-param:/s3-image-upload-app/database/username}")
-    private String dbUsernameParam;
-
-    @Value("${aws.ssm.db-password-param:/s3-image-upload-app/database/password}")
-    private String dbPasswordParam;
-
-    @Value("${spring.datasource.timeout:30000}")
-    private int connectionTimeout;
+    private final SsmClient ssmClient;
 
     public DataSourceConfig() {
-        this.ssmClient = AWSSimpleSystemsManagementClientBuilder.standard()
+        this.ssmClient = SsmClient.builder()
+                .region(Region.US_EAST_2)  // Match your AWS region
                 .build();
     }
 
     private String getParameterValue(String parameterName) {
         try {
-            GetParameterRequest request = new GetParameterRequest()
-                    .withName(parameterName)
-                    .withWithDecryption(true);
-
-            String value = ssmClient.getParameter(request).getParameter().getValue();
-            logger.info("Successfully retrieved parameter: {}", parameterName);
-            return value;
+            GetParameterRequest request = GetParameterRequest.builder()
+                    .name(parameterName)
+                    .withDecryption(true)
+                    .build();
+            return ssmClient.getParameter(request).parameter().value();
         } catch (Exception e) {
-            logger.error("Error retrieving parameter {}: {}", parameterName, e.getMessage(), e);
             throw new RuntimeException("Could not retrieve database parameter: " + parameterName, e);
         }
     }
 
     @Bean
-    @Primary
     public DataSource dataSource() {
         try {
-            // Retrieve database parameters from SSM Parameter Store
-            String endpoint = getParameterValue(dbEndpointParam);
-            String dbName = getParameterValue(dbNameParam);
-            String username = getParameterValue(dbUsernameParam);
-            String password = getParameterValue(dbPasswordParam);
+            // Retrieve database parameters from SSM
+            String endpoint = getParameterValue("/s3-image-upload-app/database/endpoint");
+            String dbName = getParameterValue("/s3-image-upload-app/database/name");
+            String username = getParameterValue("/s3-image-upload-app/database/username");
+            String password = getParameterValue("/s3-image-upload-app/database/password");
 
-            logger.info("Configuring DataSource with endpoint: {}, database: {}", endpoint, dbName);
-
+            // Construct JDBC URL with improved configuration
             String jdbcUrl = String.format(
-                    "jdbc:postgresql://%s:5432/%s?connectTimeout=%d" +
+                    "jdbc:postgresql://%s:5432/%s?connectTimeout=10000" +
                             "&ssl=true" +
                             "&sslmode=require" +
                             "&tcpKeepAlive=true" +
-                            "&socketTimeout=30" +
-                            "&applicationName=s3-image-upload-app",
-                    endpoint, dbName, connectionTimeout
+                            "&socketTimeout=30000" +
+                            "&applicationName=s3-image-upload-app" +
+                            "&loggerLevel=TRACE", // Add detailed logging
+                    endpoint, dbName
             );
 
             return DataSourceBuilder.create()
+                    .type(HikariDataSource.class)
+                    .driverClassName("org.postgresql.Driver")
                     .url(jdbcUrl)
                     .username(username)
                     .password(password)
-                    .driverClassName("org.postgresql.Driver")
                     .build();
         } catch (Exception e) {
-            logger.error("Failed to create DataSource", e);
-            throw new RuntimeException("Could not create DataSource", e);
+            throw new RuntimeException("Failed to create DataSource", e);
         }
     }
 }
