535f81b65ef947dba76a676a5c0129c84acbfaf6
nicholas <nicholas.oppong@amalitech.com>
2025-03-25 10:59:27 +0000

    build

--- src/test/java/com/example/All/in/one/AllInOneApplicationTests.java
+++ src/test/java/com/example/All/in/one/AllInOneApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.All.in.one;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class AllInOneApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Intentionally empty test to verify context loading
 	}
-
 }
